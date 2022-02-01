befacdc13de7d37cd3057839eea71c3d23c81f01
luizrafa-federico <luizrafa.federico@gmail.com>
2022-01-31 20:48:09 -0500

    fix: migrations after unsuccessful docker-compose run

--- src/database/migrations/1643679963735-initialCommit.ts
+++ src/database/migrations/1643679963735-initialCommit.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class initialCommit1643671955519 implements MigrationInterface {
-    name = 'initialCommit1643671955519'
+export class initialCommit1643679963735 implements MigrationInterface {
+    name = 'initialCommit1643679963735'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("uuid" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "createdOn" TIMESTAMP NOT NULL, "updatedOn" TIMESTAMP NOT NULL, "isAdm" boolean NOT NULL, CONSTRAINT "PK_951b8f1dfc94ac1d0301a14b7e1" PRIMARY KEY ("uuid"))`);
